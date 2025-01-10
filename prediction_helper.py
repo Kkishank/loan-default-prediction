f9006f45b2c89f3a60a090454c9ec78f1160968a
Kishan Kumar <kkishank49@outlook.com>
2025-01-10 16:44:36 +0530

    first commit

--- prediction_helper.py
+++ prediction_helper.py
@@ -28,24 +28,7 @@ def prepare_input(age, income, loan_amount, loan_tenure_months, avg_dpd_per_deli
         'avg_dpd_per_delinquency': avg_dpd_per_delinquency,
         'residence_type_Owned': 1 if residence_type == 'Owned' else 0,
         'residence_type_Rented': 1 if residence_type == 'Rented' else 0,
-        'loan_purpose_Education': 1 if loan_purpose == 'Education' else 0,
-        'loan_purpose_Home': 1 if loan_purpose == 'Home' else 0,
-        'loan_purpose_Personal': 1 if loan_purpose == 'Personal' else 0,
-        'loan_type_Unsecured': 1 if loan_type == 'Unsecured' else 0,
-        # additional dummy fields just for scaling purpose
-        'number_of_dependants': 1,  # Dummy value
-        'years_at_current_address': 1,  # Dummy value
-        'zipcode': 1,  # Dummy value
-        'sanction_amount': 1,  # Dummy value
-        'processing_fee': 1,  # Dummy value
-        'gst': 1,  # Dummy value
-        'net_disbursement': 1,  # Computed dummy value
-        'principal_outstanding': 1,  # Dummy value
-        'bank_balance_at_application': 1,  # Dummy value
-        'number_of_closed_accounts': 1,  # Dummy value
-        'enquiry_count': 1  # Dummy value
     }
-
     # Ensure all columns for features and cols_to_scale are present
     df = pd.DataFrame([input_data])
 
